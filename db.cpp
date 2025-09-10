0e7526a9a3b60e66a49df2e4ea38af4f1802e979
Gulshan1-3 <ankitstar955@gmail.com>
2025-09-10 22:05:30 +0530

    table created

--- db.cpp
+++ db.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 #include <string>
-using namespace std;
+#include <cstdint>
 
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+
+
+using namespace std;
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+class Row {
+    public:
+    u_int32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+};
+
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+class Table{
+    public:
+    uint32_t num_rows;
+  void* pages[TABLE_MAX_PAGES];
+};
+
+void print_row(Row* row) {
+std::cout << "(" << row->id << ", " 
+          << row->username << ", " 
+          << row->email << ")" << std::endl;
+}
 
 class InputBuffer {
 public:
@@ -13,7 +53,7 @@ public:
 
     bool readInput() {
         if (!std::getline(std::cin, buffer)) {
-            // If we hit EOF (Ctrl+D / Ctrl+Z), exit gracefully
+           
             return false;
         }
         input_length = buffer.size();
@@ -35,7 +75,8 @@ enum class MetaCommandResult {
 
 enum class PrepareResult {
     Success,
-    UnrecognizedStatement
+    UnrecognizedStatement,
+    SyntaxError
 };
 enum class StatementType {
  StatementInsert,
@@ -44,20 +85,36 @@ enum class StatementType {
 class Statement {
   public:
   StatementType type;
+  Row row_to_insert;
 };
 
 
-PrepareResult prepare_statement(InputBuffer& inputBuffer,Statement& statement){
-    if(inputBuffer.buffer.rfind("insert",0)==0) {
-     statement.type = StatementType::StatementInsert;
-     return PrepareResult::Success;
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if (input_buffer->buffer.rfind("insert", 6) == 0) {
+        statement->type = StatementType::StatementInsert;
+        int args_assigned = sscanf(
+            input_buffer->buffer.c_str(),
+            "insert %d %s %s",
+            &statement->row_to_insert.id,
+            statement->row_to_insert.username,
+            statement->row_to_insert.email
+        );
+
+        if (args_assigned < 3) {
+            return PrepareResult::SyntaxError;
+        }
+        return PrepareResult::Success;
     }
-    if(inputBuffer.buffer == "select") {
-     statement.type = StatementType::StatementSelect;
-          return PrepareResult::Success;
+
+    if (input_buffer->buffer == "select") {
+        statement->type = StatementType::StatementSelect;
+        return PrepareResult::Success;
     }
-       return PrepareResult::UnrecognizedStatement;
+
+    return PrepareResult::UnrecognizedStatement;
 }
+
+
 
 void execute_statement(Statement& statement) {
     switch(statement.type) {
@@ -103,9 +160,12 @@ int main() {
         }
           
       }
-      switch (prepare_statement(inputBuffer, statement)) {
+      switch (prepare_statement(&inputBuffer, &statement)) {
     case PrepareResult::Success:
         break;
+         case (PrepareResult::SyntaxError):
+	std::cout<<("Syntax error. Could not parse statement.\n");
+	continue;
     case PrepareResult::UnrecognizedStatement:
         std::cout << "Unrecognized keyword at start of '" 
                   << inputBuffer.buffer << "'.\n";
